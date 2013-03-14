b23c27cfce33a0a997a00295ff5f2e0927817511
whitehat2k9 <whitehat2k9@gmail.com>
2013-03-14 17:32:43 -0500

    Added GPL v3 licensing Bumped version number

--- version.h
+++ version.h
@@ -1,8 +1,27 @@
+/* * qMakeTorrent - Advanced torrent creator with batch capability
+ * Copyright (C) 2013 whitehat2k9 <whitehat2k9@gmail.com>
+ *
+ * This file is part of qMakeTorrent.
+ *
+ * qMakeTorrent is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qMakeTorrent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qMakeTorrent.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef VERSION_H
 #define VERSION_H
 
 #define PROGRAM_NAME "qMakeTorrent"
-#define PROGRAM_VERSION "0.95"
+#define PROGRAM_VERSION "1.0.0 RC1"
 #define PROGRAM_URL "https://github.com/whitehat2k9/qMakeTorrent"
 
 #endif
--- wizard.h
+++ wizard.h
@@ -1,3 +1,22 @@
+/* * qMakeTorrent - Advanced torrent creator with batch capability
+ * Copyright (C) 2013 whitehat2k9 <whitehat2k9@gmail.com>
+ *
+ * This file is part of qMakeTorrent.
+ *
+ * qMakeTorrent is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qMakeTorrent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qMakeTorrent.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef WIZARD_H
 #define WIZARD_H
 
